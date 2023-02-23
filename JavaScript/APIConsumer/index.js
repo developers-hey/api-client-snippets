7e45caa6096cc35249d8fcc1304634670a85a78e
fernando.cobos <fernando.cobos>
2023-02-23 08:33:55 -0600

    Se agrega mtls

--- JavaScript/APIConsumer/index.js
+++ JavaScript/APIConsumer/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const fs = require('fs');
+const tls = require('tls');
+const crypto = require('crypto');
+const https = require('https');
+const { promisify } = require('util');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const readFileAsync = promisify(fs.readFile);
+
 
 app.get('/', (req, res) => {
   const authOptions = {
@@ -20,6 +28,25 @@ app.get('/', (req, res) => {
     }
   };
 
+  async function getSSLContext() {
+    const clientCert = await readFileAsync('resources/Client_KeyStore_test_Lemus.p12');
+    const passphrase = 'Masterkey01';
+  
+    const clientKey = await crypto.createPrivateKey({
+      key: clientCert,
+      format: 'pkcs12',
+      passphrase,
+    });
+  
+    const options = {
+      key: clientKey,
+      passphrase,
+      rejectUnauthorized: true,
+    };
+  
+    return https.createSecureContext(options);
+  }
+getSSLContext();
   request.post(authOptions, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const token = body.access_token;
@@ -39,7 +66,10 @@ app.get('/', (req, res) => {
       res.send(error);
     }
   });
+
 });
+
+
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
